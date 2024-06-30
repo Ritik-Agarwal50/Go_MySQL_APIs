2d783499f2d139ca08c40d578c96151be7792385
Roble✨ <ritikagarwal570@gmail.com>
2024-06-30 16:34:29 +0530

    add

--- pkg/models/books.go
+++ pkg/models/books.go
@@ -1,1 +1,22 @@
 package models
+
+import (
+	"github.com/jinzhu/gorm"
+	"github.com/ritik-agarwal50/go-bookstore/pkg/config"
+
+)
+
+
+var db *gorm.DB
+type Book struct {
+	gorm.Model
+	Name string `gorn:""json:"name"`
+	Author string `json:"author"`
+	Publlication string `json:"publication"`
+}
+
+func init() {
+	config.Connect()
+	db = config.GetDB()
+	db.AutoMigrate(&Book{})
+}
