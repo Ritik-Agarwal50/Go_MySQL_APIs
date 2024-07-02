f17b7d4eeeff3d712b5adf2120b92a8f01bd6385
Roble✨ <ritikagarwal570@gmail.com>
2024-07-02 22:19:56 +0530

    main

--- cmd/main.go
+++ cmd/main.go
@@ -1,3 +1,13 @@
 package main
 
-func main() {}
+import (
+	"log"
+	"net/http"
+	"github.com/gorilla/mux"
+	"github.com/ritik-agarwal50/go-bookstore/pkg/routes"
+	"github.com/jinzhu/gorm/dialects/mysql"
+
+)
+func main() {
+	
+}
--- pkg/models/books.go
+++ pkg/models/books.go
@@ -20,3 +20,4 @@ func init() {
 	db = config.GetDB()
 	db.AutoMigrate(&Book{})
 }
+
