0d16fdb9d3a02af839f2eff94f7e9bb78f0ae2f1
Roble✨ <ritikagarwal570@gmail.com>
2024-07-03 09:11:38 +0530

    updated

--- pkg/controllers/book-controller.go
+++ pkg/controllers/book-controller.go
@@ -1,1 +1,36 @@
 package controller
+
+import (
+	"encoding/json"
+	"net/http"
+	"fmt"
+	"github.com/ritik-agarwal50/go-bookstore/pkg/models"
+	"github.com/gorilla/mux"
+	"strconv"
+	"github.com/ritik-agarwal50/go-bookstore/pkg/utils"
+)
+var newBook models.Book
+
+func getBook(w http.ResponseWriter, r *http.Request){
+	newBooks := models.GetAllBooks()
+	res, _ := json.Marshal(newBooks)
+	w.Header().Set("Content-Type", "pkglication/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
+
+func GetBookById(w http.ResponseWriter, r *http.Request){
+	vars := mux.Vars(r)
+	bookId := vars["bookId"]
+	Id, err := strconv.ParseInt(bookId, 0, 0)
+	if err != nil {
+		fmt.Println("Error while parsing")
+	}
+	bookDetails, _ := models.GetBookById(Id)
+	res, _ := json.Marshal(bookDetails)
+	w.Header().Set("Content-Type", "pkglication/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
+
+func CreateBook(){}
--- pkg/models/books.go
+++ pkg/models/books.go
@@ -22,6 +22,25 @@ func init() {
 }
 
 func (b *Book) CreateBook() *Book {
-	db.Create(b)
+	db.NewRecord(b)
+	db.Create(&b)
 	return b
+}
+
+func GetAllBooks() []Book {
+	var Books []Book
+	db.Find(&Books)
+	return Books
+}
+
+func GetBookById(Id int64) (*Book, *gorm.DB) {
+	var getBook Book
+	db.Where("ID = ?", Id).Find(&getBook)
+	return &getBook, db
+}
+
+func DeleteBook(ID int64) Book {
+	var book Book
+	db.Where("ID = ?", ID).Delete(book)
+	return book
 }
